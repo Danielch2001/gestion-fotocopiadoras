1a46386c6fa97ffd17bb2d7395d9dac46624a303
daniel <daniel.chicaiza@udla.edu.ec>
2025-01-05 04:06:54 -0500

    Actualización del archivo .gitignore y limpieza de rastreados previos

--- backend/routes/usuarios.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getUsuarios } = require('../controllers/usuariosController');
-
-const router = express.Router();
-
-router.get('/', getUsuarios);
-
-module.exports = router;
